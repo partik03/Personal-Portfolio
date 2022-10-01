7f52a3a5fbc7aaad31ec5d00c4aac52dfe21bfa1
Partik SIngh <partiksingh@pop-os.localdomain>
2022-10-01 23:56:05 +0530

    Almost Completed

--- components/Corousel.js
+++ components/Corousel.js
@@ -1,49 +1,60 @@
+// import img from 'next/img';
 import React, { useState } from 'react'
-import styles from "../styles/Corousel.module.css"
-export const CorouselItem=({children, width})=>{
-return(
-    <div className={styles.corousel_item} style={{width:width}}>
-        {children}
-    </div>
-)
+// import styles from '../styles/Corousel.module.css'
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import {Swiper, SwiperSlide } from "swiper/react";
+import { EffectCoverflow,Navigation,Keyboard, Autoplay, Pagination} from "swiper";
+const Corousel = () => {
+    return (
+        <>
+         <Swiper
+        effect={"coverflow"}
+        grabCursor={true}
+        centeredSlides={true}
+        slidesPerView={"auto"}
+        coverflowEffect={{
+          rotate: 50,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          slideShadows: true,
+        }}
+        pagination={{ clickable: true ,enabled:true}}
+        navigation={true}
+        keyboard={{enabled:true}}
+        loop={true}
+        autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
+        modules={[EffectCoverflow,Navigation,Keyboard,Autoplay,Pagination]}
+        className="mySwiper"
+        style={{
+        width: "100%",
+         paddingTop: "1vh",
+        paddingBottom: "1vh",}}
+      >
+        <SwiperSlide style={{ 
+        backgroundPosition: "center",
+        backgroundSize: "cover",
+        width: "100%",
+        height: "60vh",
+        }}>
+          <img src="https://englishtribuneimages.blob.core.windows.net/gallary-content/2022/5/2022_5$largeimg_837079638.jpg" alt="5" />
+        </SwiperSlide>
+        <SwiperSlide style={{ backgroundPosition: "center",
+        backgroundSize: "cover",
+        width: "100%",
+        height: "70vh",
+        }}>
+          <img src="https://englishtribuneimages.blob.core.windows.net/gallary-content/2022/5/2022_5$largeimg_837079638.jpg" alt="6" />
+        </SwiperSlide>
+      </Swiper>
+        </>
+    )
+  
 }
-const Corousel = ({children}) => {
-    const [active, setActive] = useState(0)
-    function updateIndex(newIndex) {
-            if(newIndex < 0){
-                newIndex = 0;
-        }
-        else if(newIndex >= React.Children.count(children)){
-            newIndex = React.Children.count(children) -1;
-        }
-        setActive(newIndex);
-    }
-  return (
-    <div className={styles.corousel}>
-<div className={styles.inner} style={
-    {
-        transform:`translateX(-${active * 100}%)`
-    }
-}>
-    {
-        React.Children.map(children ,(child,index)=>{
-            return React.cloneElement(child,{width:"100%"});
-        })
-    }
-</div>
-<div className={styles.indicators}>
-    <button className={styles.left}
-    onClick={()=>{updateIndex(active-1)}}
-    >
-        &lt;Prev
-    </button>
-    <button className={styles.right}
-    onClick={()=>{updateIndex(active+1)}}>
-    Next&gt;
-    </button>
-</div>
-    </div>
-  )
-}
-
 export default Corousel
---
+++ components/Skills.js
@@ -0,0 +1,272 @@
+export default function skills(){
+    return(
+        <>
+        <a href="https://www.gnu.org/software/bash/" target="_blank" rel="noreferrer">
+                <img
+                  src="https://www.vectorlogo.zone/logos/gnu_bash/gnu_bash-icon.svg"
+                  alt="bash"
+                  width="8%"
+                  height="10%"
+                />
+              </a>
+              <a href="https://getbootstrap.com" target="_blank" rel="noreferrer">
+                <img
+                  src="https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-plain-wordmark.svg"
+                  alt="bootstrap"
+                  width="8%"
+                  height="10%"
+                />
+              </a>
+              <a href="https://www.cprogramming.com/" target="_blank" rel="noreferrer">
+                <img
+                  src="https://raw.githubusercontent.com/devicons/devicon/master/icons/c/c-original.svg"
+                  alt="c"
+                  width="8%"
+                  height="10%"
+                />{" "}
+              </a>
+              <a href="https://www.w3schools.com/css/" target="_blank" rel="noreferrer">
+                <img
+                  src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg"
+                  alt="css3"
+                  width="8%"
+                  height="10%"
+                />
+              </a>
+              <a href="https://www.djangoproject.com/" target="_blank" rel="noreferrer">
+                <img
+                  src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/django/django-plain-wordmark.svg"
+                  alt="django"
+                  width="8%"
+                  height="10%"
+                />
+              </a>
+            {" "}
+              <a href="https://expressjs.com" target="_blank" rel="noreferrer">
+                <img
+                  src="https://raw.githubusercontent.com/devicons/devicon/master/icons/express/express-original-wordmark.svg"
+                  alt="express"
+                  width="8%"
+                  height="10%"
+                />
+              </a>
+              <a href="https://firebase.google.com/" target="_blank" rel="noreferrer">
+                <img
+                  src="https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg"
+                  alt="firebase"
+                  width="8%"
+                  height="10%"
+                />
+              </a>
+              <a href="https://git-scm.com/" target="_blank" rel="noreferrer">
+                <img
+                  src="https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg"
+                  alt="git"
+                  width="8%"
+                  height="10%"
+                />
+              </a>
+              <a href="https://heroku.com" target="_blank" rel="noreferrer">
+                <img
+                  src="https://www.vectorlogo.zone/logos/heroku/heroku-icon.svg"
+                  alt="heroku"
+                  width="8%"
+                  height="10%"
+                />
+              </a>
+              <a href="https://www.w3.org/html/" target="_blank" rel="noreferrer">
+                <img
+                  src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg"
+                  alt="html5"
+                  width="8%"
+                  height="10%"
+                />{" "}
+              </a>{" "}
+              <a
+                href="https://developer.mozilla.org/en-US/docs/Web/JavaScript"
+                target="_blank" rel="noreferrer"
+              >
+                {" "}
+                <img
+                  src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg"
+                  alt="javascript"
+                  width="8%"
+                  height="10%"
+                />{" "}
+              </a>{" "}
+              <a href="https://www.linux.org/" target="_blank" rel="noreferrer">
+                {" "}
+                <img
+                  src="https://raw.githubusercontent.com/devicons/devicon/master/icons/linux/linux-original.svg"
+                  alt="linux"
+                  width="8%"
+                  height="10%"
+                />{" "}
+              </a>{" "}
+              <a href="https://www.mongodb.com/" target="_blank" rel="noreferrer">
+                {" "}
+                <img
+                  src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg"
+                  alt="mongodb"
+                  width="8%"
+                  height="10%"
+                />{" "}
+              </a>{" "}
+              <a href="https://www.mysql.com/" target="_blank" rel="noreferrer">
+                {" "}
+                <img
+                  src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg"
+                  alt="mysql"
+                  width="8%"
+                  height="10%"
+                />{" "}
+              </a>{" "}
+              <a href="https://nextjs.org/" target="_blank" rel="noreferrer">
+                {" "}
+                <img
+                  src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original.svg" 
+                  alt="nextjs"
+                  width="8%"
+                  height="10%"
+                />{" "}
+              </a>{" "}
+              <a href="https://nodejs.org" target="_blank" rel="noreferrer">
+                {" "}
+                <img
+                  src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg"
+                  alt="nodejs"
+                  width="8%"
+                  height="10%"
+                />{" "}
+              </a>{" "}
+              <a href="https://nuxtjs.org/" target="_blank" rel="noreferrer">
+                {" "}
+                <img
+                  src="https://www.vectorlogo.zone/logos/nuxtjs/nuxtjs-icon.svg"
+                  alt="nuxtjs"
+                  width="8%"
+                  height="10%"
+                />{" "}
+              </a>{" "}
+              <a href="https://postman.com" target="_blank" rel="noreferrer">
+                {" "}
+                <img
+                  src="https://www.vectorlogo.zone/logos/getpostman/getpostman-icon.svg"
+                  alt="postman"
+                  width="8%"
+                  height="10%"
+                />{" "}
+              </a>{" "}
+              <a href="https://www.python.org" target="_blank" rel="noreferrer">
+                {" "}
+                <img
+                  src="https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg"
+                  alt="python"
+                  width="8%"
+                  height="10%"
+                />{" "}
+              </a>{" "}
+              <a href="https://reactjs.org/" target="_blank" rel="noreferrer">
+                {" "}
+                <img
+                  src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg"
+                  alt="react"
+                  width="8%"
+                  height="10%"
+                />{" "}
+              </a>{" "}
+              <a href="https://www.sqlite.org/" target="_blank" rel="noreferrer">
+                {" "}
+                <img
+                  src="https://www.vectorlogo.zone/logos/sqlite/sqlite-icon.svg"
+                  alt="sqlite"
+                  width="8%"
+                  height="10%"
+                />{" "}
+              </a>{" "}
+              <a href="https://tailwindcss.com/" target="_blank" rel="noreferrer">
+                {" "}
+                <img
+                  src="https://www.vectorlogo.zone/logos/tailwindcss/tailwindcss-icon.svg"
+                  alt="tailwind"
+                  width="8%"
+                  height="10%"
+                />{" "}
+              </a>{" "}
+              <a href="https://www.typescriptlang.org/" target="_blank" rel="noreferrer">
+                {" "}
+                <img
+                  src="https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg"
+                  alt="typescript"
+                  width="8%"
+                  height="10%"
+                />{" "}
+              </a>{" "}
+              <a href="https://vuejs.org/" target="_blank" rel="noreferrer">
+                {" "}
+                <img
+                  src="https://raw.githubusercontent.com/devicons/devicon/master/icons/vuejs/vuejs-original-wordmark.svg"
+                  alt="vuejs"
+                  width="8%"
+                  height="10%"
+                />{" "}
+              </a>{" "}
+              <a href="https://vuetifyjs.com/en/" target="_blank" rel="noreferrer">
+                {" "}
+                <img
+                  src="https://bestofjs.org/logos/vuetify.svg"
+                  alt="vuetify"
+                  width="8%"
+                  height="10%"
+                />{" "}
+              </a>{" "}
+              <a href="https://docs.soliditylang.org/en/v0.8.17/">
+
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/solidity/solidity-original.svg" alt="solidity"
+             width="8%"
+             height="10%"
+            />
+          
+              </a>
+              <a href="https://www.rust-lang.org/">
+
+            <img  src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/rust/rust-plain.svg" 
+            alt="rust"
+             width="8%"
+             height="10%"
+            />
+          
+              </a>
+              <a href="https://go.dev/">
+
+           
+              <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/go/go-original.svg" 
+            alt="go"
+             width="8%"
+             height="10%"
+            />
+          
+              </a>
+              <a href="https://npmjs.com/">
+
+           
+              <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/npm/npm-original-wordmark.svg" 
+            alt="go"
+             width="8%"
+             height="10%"
+            />
+          
+              </a>
+              <a href="https://yarnpkg.com/">
+
+           
+              <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/yarn/yarn-original-wordmark.svg" 
+            alt="go"
+             width="8%"
+             height="10%"
+            />
+          
+              </a>
+        </>
+    )
+}
